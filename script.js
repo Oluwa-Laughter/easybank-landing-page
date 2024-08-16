3ffbfd25a26dbdce675fa9eb17574fca25b8b5d6
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-08-16 21:02:06 +0100

    initialize repo

---
+++ script.js
@@ -0,0 +1,26 @@
+"use-strict";
+const btnMobileNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+const btnNavMenu = function (e) {
+  e.preventDefault();
+  header.classList.toggle("nav-open");
+};
+
+btnMobileNav.addEventListener("click", btnNavMenu);
+
+const heroSection = document.querySelector(".hero-section");
+const headerHeight = header.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) document.body.classList.add("sticky");
+  else document.body.classList.remove("sticky");
+};
+const stickyObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 1,
+  rootMargin: `${headerHeight}px`,
+});
+
+stickyObserver.observe(heroSection);
